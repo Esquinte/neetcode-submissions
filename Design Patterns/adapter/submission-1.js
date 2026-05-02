6022ae1f0ca973e3c2d223c9c5b53ad6a60db72f
Benjamin Durand <94durandbenjamin@gmail.com>
2026-05-02 21:55:40 +0200

    Add: adapter - submission-1

---
+++ Design Patterns/adapter/submission-1.js
@@ -0,0 +1,64 @@
+class SquareHole {
+    /**
+     * @param {number} sideLength
+     */
+    constructor(sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    /**
+     * @param {Square} square
+     * @return {boolean}
+     */
+    canFit(square) {
+        return this.sideLength >= square.getSideLength();
+    }
+}
+
+class Square {
+    /**
+     * @param {number} sideLength
+     */
+    constructor(sideLength = 0) {
+        this.sideLength = sideLength;
+    }
+
+    /**
+     * @return {number}
+     */
+    getSideLength() {
+        return this.sideLength;
+    }
+}
+
+class Circle {
+    /**
+     * @param {number} radius
+     */
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * @return {number}
+     */
+    getRadius() {
+        return this.radius;
+    }
+}
+
+class CircleToSquareAdapter extends Square {
+    /**
+     * @param {Circle} circle
+     */
+    constructor(circle) {
+        super(circle.radius * 2)
+    }
+
+    /**
+     * @return {number}
+     */
+    getSideLength() {
+        return super.getSideLength()
+    }
+}
