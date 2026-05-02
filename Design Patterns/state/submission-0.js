1ef5d760f6287791f330f48a4ef3eb0b42a4afe0
Benjamin Durand <94durandbenjamin@gmail.com>
2026-05-02 23:23:57 +0200

    Add: state - submission-0

---
+++ Design Patterns/state/submission-0.js
@@ -0,0 +1,75 @@
+class State {
+    /**
+     * @param {Document} doc
+     */
+    handleRequest(doc) {
+        throw new Error("Method 'handleRequest()' must be implemented.");
+    }
+}
+
+class Document {
+    constructor() {
+        this.state = new Draft();
+        this.approved = false;
+    }
+
+    /**
+     * @return {State}
+     */
+    getState() {
+        return this.state;
+    }
+
+    /**
+     * @param {State} state
+     */
+    setState(state) {
+        this.state = state;
+    }
+
+    publish() {
+        this.state.handleRequest(this);
+    }
+
+    /**
+     * @param {boolean} approved
+     */
+    setApproval(approved) {
+        this.approved = approved;
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isApproved() {
+        return this.approved;
+    }
+}
+
+class Draft extends State {
+    /**
+     * @param {Document} doc
+     * @return {void}
+     */
+    handleRequest(doc) {
+        doc.setState(new Review())
+    }
+}
+
+class Review extends State {
+    /**
+     * @param {Document} doc
+     * @return {void}
+     */
+    handleRequest(doc) {
+        doc.setState(doc.isApproved() ? new Published() : new Draft());
+    }
+}
+
+class Published extends State {
+    /**
+     * @param {Document} doc
+     * @return {void}
+     */
+    handleRequest(doc) {}
+}
