9f67a4bfc0406e65a236490c360655c9af6ca542
Benjamin Durand <94durandbenjamin@gmail.com>
2026-05-18 11:13:54 +0200

    Add: sql-highest-grade-for-each-student - submission-1

---
+++ SQL/sql-highest-grade-for-each-student/submission-1.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT
+    DISTINCT ON (er.student_id)
+    er.student_id,
+    er.exam_id,
+    er.score
+FROM exam_results er
+ORDER BY
+    er.student_id ASC,
+    er.score DESC,
+    er.exam_id ASC;
