2871f623520ab8dbe93bf9769412f0ec1f76d357
Benjamin Durand <94durandbenjamin@gmail.com>
2026-05-22 10:26:53 +0200

    Add: sql-consecutive-available-seats - submission-1

---
+++ SQL/sql-consecutive-available-seats/submission-1.sql
@@ -0,0 +1,15 @@
+-- Write your query below
+SELECT s.seat_id FROM (
+    SELECT
+        c.seat_id,
+        c.free,
+        LAG(c.free) OVER (
+            ORDER BY c.seat_id
+        ) AS previous_free,
+        LEAD(c.free) OVER (
+            ORDER BY c.seat_id
+        ) AS next_free
+    FROM cinema c
+    ORDER BY c.seat_id
+) s
+WHERE s.free = 1 AND (s.previous_free = 1 OR s.next_free = 1);
