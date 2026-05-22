132807ff4886cf055e2b8fd4634632422309237d
Benjamin Durand <94durandbenjamin@gmail.com>
2026-05-22 10:44:44 +0200

    Add: sql-rearrange-products-table - submission-0

---
+++ SQL/sql-rearrange-products-table/submission-0.sql
@@ -0,0 +1,27 @@
+-- Write your query below
+SELECT
+    p.product_id,
+    'store1' AS store,
+    p.store1 AS price
+FROM products p
+WHERE p.store1 IS NOT NULL
+
+UNION ALL
+
+SELECT
+    p.product_id,
+    'store2' AS store,
+    p.store2 AS price
+FROM products p
+WHERE p.store2 IS NOT NULL
+
+UNION ALL
+
+SELECT
+    p.product_id,
+    'store3' AS store,
+    p.store3 AS price
+FROM products p
+WHERE p.store3 IS NOT NULL
+
+ORDER BY product_id ASC, store ASC;
