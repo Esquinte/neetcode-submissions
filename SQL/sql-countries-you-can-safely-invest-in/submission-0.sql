bf6193a43d8dbd41a978d546ff4875215951c3c4
Benjamin Durand <94durandbenjamin@gmail.com>
2026-05-22 10:15:00 +0200

    Add: sql-countries-you-can-safely-invest-in - submission-0

---
+++ SQL/sql-countries-you-can-safely-invest-in/submission-0.sql
@@ -0,0 +1,10 @@
+-- Write your query below
+SELECT
+    co.name AS country
+FROM country co
+    JOIN person p ON p.phone_number like CONCAT(co.country_code, '%')
+    JOIN calls ca ON p.id = ca.caller_id OR p.id = ca.callee_id
+GROUP BY co.name
+HAVING ROUND(1.0 * SUM(ca.duration) / COUNT(*), 3) > (
+    SELECT AVG(duration) FROM calls
+);
