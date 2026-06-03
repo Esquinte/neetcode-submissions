3ebab9e3799666912c30a8b46895c7277cfe3e6f
Benjamin Durand <94durandbenjamin@gmail.com>
2026-06-03 23:17:15 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const match: Record<number, number> = {}
+
+        for (const num of nums) {
+            match[num] = (match[num] ?? 0)  + 1
+        }
+
+        const arr = Object.entries(match).map(([num, freq]) => [
+            freq, parseInt(num)
+        ])
+        arr.sort((a, b) => b[0] - a[0])
+
+        return arr.slice(0, k).flatMap((pair) => pair[1])
+    }
+}
