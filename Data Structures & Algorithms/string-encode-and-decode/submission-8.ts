4a32e74e6b6fb1662fdf043aec1a5b1c3a7b617c
Benjamin Durand <94durandbenjamin@gmail.com>
2026-06-08 16:13:10 +0200

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        return strs.map((str) => `${str.length}#${str}`).join("");
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const results: string[] = [];
+
+        let i = 0;
+        while (i < str.length) {
+            let j = i;
+            while (str[j] !== "#") {
+                j++;
+            }
+
+            const num = Number(str.substring(i, j));
+
+            i = j + 1;
+            j = i + num;
+
+            results.push(str.substring(i, j));
+
+            i = j;
+        }
+
+        return results;
+    }
+}
