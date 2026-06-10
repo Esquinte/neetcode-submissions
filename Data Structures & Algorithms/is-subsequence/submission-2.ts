50fcb366ede7830b29f49ba2d05e23745351e4a7
Benjamin Durand <94durandbenjamin@gmail.com>
2026-06-10 10:44:20 +0200

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s: string, t: string): boolean {
+        let ptrS = 0;
+        let ptrT = 0;
+
+        while (ptrT < t.length && ptrS < s.length) {
+            if (s[ptrS] === t[ptrT]) {
+                ptrS++;
+            }
+            ptrT++;
+        }
+
+        return ptrS === s.length;
+    }
+}
